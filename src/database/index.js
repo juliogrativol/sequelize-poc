2f147502b4b564b71f19d361e0af1d7a327d79c5
jthomaz <jthomaz@brasilcap.com.br>
2020-02-17 12:48:29 -0300

    Adding 1 to many relationship

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,13 @@
 const Sequelize = require('sequelize')
 const dbConfir = require('../config/database')
 const User = require('../models/User')
+const Address = require('../models/Address')
 
 const connection = new Sequelize(dbConfir)
 
 User.init(connection)
+Address.init(connection)
+Address.associate(connection.models)
+User.associate(connection.models)
 
 module.exports = connection
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const routes = express.Router()
 const UserController = require('./controller/UserController')
-const User = require('./models/User')
+const AddressController = require('./controller/AddressController')
 
 routes.get('/', (req, res) => { return res.json({ hello: 'world' }) })
 
 routes.post('/users', UserController.store)
 routes.get('/users', UserController.list)
+
+routes.post('/users/:user_id/addresses', AddressController.store)
+routes.get('/users/:user_id/addresses', AddressController.list)
 
 module.exports = routes
